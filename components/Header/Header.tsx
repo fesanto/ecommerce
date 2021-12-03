59793e1a5c9f3efd1913b4b102fd2b2557160a71
fesanto <fgesanto@gmail.com>
2021-12-03 11:01:14 -0300

    #6 components provider criado

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,27 +3,30 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 const SPAN = styled.span`
-color: pink;
+color: ${(props) => props.theme.color.c2a};
+font-weight: 700; 
+font: ${(props) => props.theme.font.s4underline};
 `
 
 const H3 = styled.h3` 
 color: ${(props) => props.theme.color.c4a};
 font-size: 14px;
 ${SPAN}{
-  color: green;
+  color: yellow;
 }
 `
 const H4 = styled(H3)`
-  font-size: 10px;
+  font-size: 12px;
+  color: #2222f8;
 `
 
 
 export const Header: React.FC = () => {
   return (
     <header>
-      <H3>Logo E-commerce<SPAN>Hello</SPAN></H3>
+      <H3>Logo E-commerce<SPAN>Sou um span dentro do H3</SPAN></H3>
       <H4 as="h4">TESTE</H4>
-      <SPAN>Hello</SPAN>
+      <SPAN>Ora, ora...</SPAN>
       <ul>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/components">Components</Link></li>
